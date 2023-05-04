0700eff4ecf48d6cf57778ac3c12cd7a4a69e728
Grigory Zhovtun <grigory.zhovtun@gmail.com>
2023-05-04 22:38:26 +0500

    add cases for usersReducer

---
+++ src/redux/users-reducer.ts
@@ -0,0 +1,48 @@
+import {PostsDataType} from "./store";
+
+const FOLLOW = "FOLLOW"
+const UNFOLLOW = "UNFOLLOW"
+
+type LocationType = {
+    country: string
+    city: string
+}
+
+type UserType = {
+    id: number
+    followed: boolean
+    fullName: string
+    status: string
+    location: LocationType
+}
+
+const users: UserType[] = [
+    {id: 1, followed: true, fullName: "Grigory", status: 'married', location: {country: 'Russia', city: 'Saint-Petersburg'}},
+    {id: 1, followed: false, fullName: "Sergey", status: 'married', location: {country: 'Russia', city: 'Moscow'}},
+    {id: 1, followed: true, fullName: "James", status: 'married', location: {country: 'USA', city: 'New York'}},
+]
+const initialState = {
+    users: users
+}
+
+const usersReducer = (state = initialState, action: any) => {
+    switch (action.type) {
+        case FOLLOW:
+            return {
+                ...state,
+                users: state.users.map(user => user.id === action.userId ? {...user, followed: true} : {...user})
+            }
+        case UNFOLLOW:
+            return {
+                ...state,
+                users: state.users.map(user => user.id === action.userId ? {...user, followed: false} : {...user})
+            }
+        default:
+            return state;
+    }
+}
+
+export const followAC = (userId: number) => ({type: FOLLOW, userId})
+export const unFollowAC = (userId: number) => ({type: UNFOLLOW, userId})
+
+export default usersReducer;
