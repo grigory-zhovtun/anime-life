26ba03d05974efcaed1766f4484be2368befe1e1
Grigory Zhovtun <grigory.zhovtun@gmail.com>
2023-07-17 21:50:32 +0500

    create getUsers() inside Users component

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -11,25 +11,30 @@ type UsersPropsType = {
 
 const Users = (props: UsersPropsType) => {
 
-    if(props.users.length === 0) {
+    let getUsers = () => {
+        if (props.users.length === 0) {
 
-        axios
-            .get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                props.setUsers(response.data.items)
-            })
+            axios
+                .get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsers(response.data.items)
+                })
+        }
     }
- 
+
+
+
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {props.users.map(user => {
                 return (
                     <div key={user.id}>
                         <span>
                             <div>
-                                {user.followed ? 
-                                <button onClick={() => props.unfollow(user.id)}>Unfollowed</button> : 
-                                <button onClick={() => props.follow(user.id)}>Followed</button>}
+                                {user.followed ?
+                                    <button onClick={() => props.unfollow(user.id)}>Unfollowed</button> :
+                                    <button onClick={() => props.follow(user.id)}>Followed</button>}
                             </div>
                         </span>
                         <span>
