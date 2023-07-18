7422c7f1009c6683a9d87f42fa3a804409d59bb2
Grigory Zhovtun <grigory.zhovtun@gmail.com>
2023-07-18 22:26:48 +0500

    create Users class component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -21,6 +21,7 @@ export const Navbar = () => {
         <div className={s.item}><NavLink to="/profile" className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink></div>
         <div className={s.item}><NavLink to="/dialogs" className = { navData => navData.isActive ? s.active : s.item }>Messages</NavLink></div>
         <div className={s.item}><NavLink to="/news" className = { navData => navData.isActive ? s.active : s.item }>news</NavLink></div>
+        <div className={s.item}><NavLink to="/users" className = { navData => navData.isActive ? s.active : s.item }>users</NavLink></div>
         <div className={s.item}><NavLink to="/music" className = { navData => navData.isActive ? s.active : s.item }>music</NavLink></div>
         <div className={s.item}><NavLink to="/settings" className = { navData => navData.isActive ? s.active : s.item }>Settings</NavLink></div> 
       </div>
