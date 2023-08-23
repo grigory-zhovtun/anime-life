11ffbaa8523315fd2c7e73012cc92b5b255c58e9
grigory-zhovtun <grigory.zhovtun@gmail.com>
2023-08-23 19:42:28 +0500

    fix users link

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -20,8 +20,9 @@ export const Navbar = () => {
       <div className={s.menu}>
         <div className={s.item}><NavLink to="/profile" className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink></div>
         <div className={s.item}><NavLink to="/dialogs" className = { navData => navData.isActive ? s.active : s.item }>Messages</NavLink></div>
-        <div className={s.item}><NavLink to="/news" className = { navData => navData.isActive ? s.active : s.item }>news</NavLink></div>
-        <div className={s.item}><NavLink to="/music" className = { navData => navData.isActive ? s.active : s.item }>music</NavLink></div>
+        <div className={s.item}><NavLink to="/users" className = { navData => navData.isActive ? s.active : s.item }>Users</NavLink></div>
+        <div className={s.item}><NavLink to="/news" className = { navData => navData.isActive ? s.active : s.item }>News</NavLink></div>
+        <div className={s.item}><NavLink to="/music" className = { navData => navData.isActive ? s.active : s.item }>Music</NavLink></div>
         <div className={s.item}><NavLink to="/settings" className = { navData => navData.isActive ? s.active : s.item }>Settings</NavLink></div> 
       </div>
     </nav>
